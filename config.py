17c6f5851c6d3c1a6413f2f4ca65240e0ff3ad77
ali-2265 <pakma2265@gmail.com>
2026-08-15 07:50:49 +0430

    config.py

--- config.py
+++ config.py
@@ -4,13 +4,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
-    API_ID = int(os.getenv('API_ID', 0))
-    API_HASH = os.getenv('API_HASH')
+    BOT_TOKEN = os.getenv('8541453435:AAEqXEyRE46CydJBPMPoKc87YwmCAHZWP54')
+    API_ID = int(os.getenv('34855392  
+', 0))
+    API_HASH = os.getenv('5e40d435847009c31c24042e2a3c0d3b')
     
-    # Proxy settings (Germany)
-    PROXY_HOST = os.getenv('PROXY_HOST', 'de.proxy.example.com')
-    PROXY_PORT = int(os.getenv('PROXY_PORT', 1080))
+    # Proxy settings (Germany) - با خطاگیری
+    PROXY_HOST = os.getenv('PROXY_HOST')
+    PROXY_PORT = os.getenv('PROXY_PORT')
+    
+    # اگر پروکسی پورت خالی بود یا وجود نداشت، از None استفاده کن
+    if PROXY_PORT:
+        try:
+            PROXY_PORT = int(PROXY_PORT)
+        except ValueError:
+            PROXY_PORT = None
+    else:
+        PROXY_PORT = None
+    
     PROXY_USERNAME = os.getenv('PROXY_USERNAME', '')
     PROXY_PASSWORD = os.getenv('PROXY_PASSWORD', '')
     
